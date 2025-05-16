40692c2a2f6f6194f68014a13926e9be61c5cca1
joel04kayy <jkayyala@uoguelph.ca>
2025-05-16 01:55:50 -0400

    Update resume button to use resumeIcon.png

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@
 import GradientBackground from './components/GradientBackground'
 import Navbar from './components/Navbar'
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import Image from 'next/image'
 
 export default function Home() {
   const [text] = useTypewriter({
@@ -55,9 +56,13 @@ export default function Home() {
             </svg>
           </a>
           <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="social-button resume">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/>
-            </svg>
+            <Image
+              src="/resumeIcon.png"
+              alt="Resume"
+              width={24}
+              height={24}
+              className="w-6 h-6"
+            />
           </a>
         </div>
       </div>
